a8f6da61b03ded0fb4209d3b83c404bc0b13541b
khushboo <159601832+khusboo-pundir@users.noreply.github.com>
2025-07-28 11:10:36 +0530

    Update FindAll.jsx

--- my-client/src/components/FindAll.jsx
+++ my-client/src/components/FindAll.jsx
@@ -7,7 +7,7 @@
     async function FindAllHandler(e){
         e.preventDefault();
         try{
-            const response = await axios.get("http://localhost:3002/api/employees");
+            const response = await axios.get("https://backend-ivg6.onrender.com/api/employees");
             setEmployees(response.data);
         }
         catch(err){
